4fafb7f86c07be0154e930693aed22d5115d8959
Robert Vari <mail.pythonsuli@gmail.com>
2021-07-25 17:16:35 +0200

    update

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ import sys
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QObject
+
+
+class DataManager(QObject):
+    def __init__(self):
+        super(DataManager, self).__init__()
 
 
 class RegistrationForm:
     def __init__(self):
         self.app = QGuiApplication(sys.argv)
         self.engine = QQmlApplicationEngine()
+
+
+
         self.engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
         if not self.engine.rootObjects():
             sys.exit(-1)
